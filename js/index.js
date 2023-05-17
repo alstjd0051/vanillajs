5333b1fb8c7486a49a96d73bbaa97217bdb40152
alstjd0051 <wsc03002@naver.com>
2023-05-18 02:01:46 +0900

    UPDATE:: recipe

--- js/index.js
+++ js/index.js
@@ -1,106 +1,96 @@
-const image = document.getElementById("cover"),
-  title = document.getElementById("music-title"),
-  artist = document.getElementById("music-artist"),
-  currentTimeEl = document.getElementById("current-time"),
-  durationEl = document.getElementById("duration"),
-  progress = document.getElementById("progress"),
-  playerProgress = document.getElementById("player-progress"),
-  prevBtn = document.getElementById("prev"),
-  nextBtn = document.getElementById("next"),
-  playBtn = document.getElementById("play"),
-  background = document.getElementById("bg-img");
+const resultContainer = document.getElementById("result");
+const searchBtn = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+const searchContainer = document.querySelector(".search-box");
 
-const music = new Audio();
+// Api url to fetch meal data
+const apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
-const songs = [
-  {
-    path: "/asset/1.mp3",
-    displayName: "The Charmer's Call",
-    cover: "/asset/1.jpg",
-    artist: "Hanu Dixit",
-  },
-  {
-    path: "/asset/2.mp3",
-    displayName: "You Will Never See Me Coming",
-    cover: "/asset/2.jpg",
-    artist: "NEFFEX",
-  },
-  {
-    path: "/asset/3.mp3",
-    displayName: "Intellect",
-    cover: "/asset/3.jpg",
-    artist: "Yung Logos",
-  },
-];
-
-let musicIndex = 0;
-let isPlaying = false;
-
-function togglePlay() {
-  if (isPlaying) {
-    pauseMusic();
-  } else {
-    playMusic();
+// Event listeners for search and input (when press enter)
+searchBtn.addEventListener("click", searchMeal);
+searchInput.addEventListener("keydown", function (e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    searchMeal();
   }
+});
+
+// Handle meal function
+function searchMeal() {
+  const userInput = searchInput.value.trim();
+  if (!userInput) {
+    resultContainer.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
+    return;
+  }
+  // Fetch meal data using api with user input
+  fetch(apiUrl + userInput)
+    .then((response) => response.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      // Handle where no meal found
+      if (!meal) {
+        resultContainer.innerHTML = `<h3>No Meal Found, Please Try Again!</h3>`;
+        return;
+      }
+      const ingredients = getIngredients(meal);
+      // Generate Html to display meal data
+      const recipeHtml = `
+            <div class="details">
+                <h2>${meal.strMeal}</h2>
+                <h4>${meal.strArea}</h4>
+            </div>
+            <img src=${meal.strMealThumb} alt=${meal.strMeal} />
+            <div id="ingre-container">
+                <h3>Ingredients:</h3>
+                <ul>${ingredients}</ul>
+            </div>
+            <div id="recipe">
+                <button id="hide-recipe">X</button>
+                <pre id="instructions">${meal.strInstructions}</pre>
+            </div>
+            <button id="show-recipe">View Recipe</button>
+        `;
+      resultContainer.innerHTML = recipeHtml;
+
+      const hideRecipeBtn = document.getElementById("hide-recipe");
+      hideRecipeBtn.addEventListener("click", hideRecipe);
+      const showRecipeBtn = document.getElementById("show-recipe");
+      showRecipeBtn.addEventListener("click", showRecipe);
+      searchContainer.style.opacity = "0";
+      searchContainer.style.display = "none";
+    })
+    // Handle error
+    .catch(() => {
+      searchContainer.style.opacity = "1";
+      searchContainer.style.display = "grid";
+      resultContainer.innerHTML = `<h3>Error fetching data!</h3>`;
+    });
 }
 
-function playMusic() {
-  isPlaying = true;
-  // Change play button icon
-  playBtn.classList.replace("fa-play", "fa-pause");
-  // Set button hover title
-  playBtn.setAttribute("title", "Pause");
-  music.play();
+// Generate html for list of ingredients
+function getIngredients(meal) {
+  let ingreHtml = "";
+  // There can be maximum of 20 ingredients
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = meal[`strIngredient${i}`];
+    if (ingredient) {
+      const measure = meal[`strMeasure${i}`];
+      ingreHtml += `<li>${measure} ${ingredient}</li>`;
+    }
+    // If ingredient doesn't exist, exit loop
+    else {
+      break;
+    }
+  }
+  return ingreHtml;
 }
 
-function pauseMusic() {
-  isPlaying = false;
-  // Change pause button icon
-  playBtn.classList.replace("fa-pause", "fa-play");
-  // Set button hover title
-  playBtn.setAttribute("title", "Play");
-  music.pause();
+// Handle show and hide recipe for a meal
+function hideRecipe() {
+  const recipe = document.getElementById("recipe");
+  recipe.style.display = "none";
 }
-
-function loadMusic(song) {
-  music.src = song.path;
-  title.textContent = song.displayName;
-  artist.textContent = song.artist;
-  image.src = song.cover;
-  background.src = song.cover;
+function showRecipe() {
+  const recipe = document.getElementById("recipe");
+  recipe.style.display = "block";
 }
-
-function changeMusic(direction) {
-  musicIndex = (musicIndex + direction + songs.length) % songs.length;
-  loadMusic(songs[musicIndex]);
-  playMusic();
-}
-
-function updateProgressBar() {
-  const { duration, currentTime } = music;
-  const progressPercent = (currentTime / duration) * 100;
-  progress.style.width = `${progressPercent}%`;
-
-  const formatTime = (time) => String(Math.floor(time)).padStart(2, "0");
-  durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(
-    duration % 60
-  )}`;
-  currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(
-    currentTime % 60
-  )}`;
-}
-
-function setProgressBar(e) {
-  const width = playerProgress.clientWidth;
-  const clickX = e.offsetX;
-  music.currentTime = (clickX / width) * music.duration;
-}
-
-playBtn.addEventListener("click", togglePlay);
-prevBtn.addEventListener("click", () => changeMusic(-1));
-nextBtn.addEventListener("click", () => changeMusic(1));
-music.addEventListener("ended", () => changeMusic(1));
-music.addEventListener("timeupdate", updateProgressBar);
-playerProgress.addEventListener("click", setProgressBar);
-
-loadMusic(songs[musicIndex]);
